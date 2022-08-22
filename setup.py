1472d5c3a55c7d3f7b14f9b481a7ce2a71a62e93
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2022-08-22 19:42:03 +0300

    Migrate setup.py options to setup.cfg
    Modernize the Python packaging my moving most of the metadata into
    setup.cfg.

--- setup.py
+++ setup.py
@@ -1,26 +1,9 @@
 from setuptools import setup
 
 
-DESCRIPTION = (
-    "A simple wrapper for running Winetricks commands for Proton-enabled "
-    "games."
-)
-LONG_DESCRIPTION = (
-    "A simple wrapper for running Winetricks commands for Proton-enabled "
-    "games. Protontricks requires Winetricks."
-)
-AUTHOR = "Janne Pulkkinen"
-AUTHOR_EMAIL = "janne.pulkkinen@protonmail.com"
-URL = "https://github.com/Matoking/protontricks"
-
-
 setup(
-    name="protontricks",
-    use_scm_version={
-        "write_to": "src/protontricks/_version.py"
-    },
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    # This is considered deprecated since Python wheels don't provide a way
+    # to install package-related files outside the package directory
     data_files=[
         (
             "share/applications",
@@ -31,40 +14,5 @@ setup(
                  "protontricks-launch.desktop")
             ]
         )
-    ],
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    python_requires=">=3.6",
-    url=URL,
-    packages=["protontricks"],
-    package_data={"": ["LICENSE"]},
-    package_dir={"protontricks": "src/protontricks"},
-    install_requires=[
-        "setuptools",  # Required for pkg_resources
-        "vdf>=3.2"
-    ],
-    entry_points={
-        "console_scripts": [
-            "protontricks = protontricks.cli.main:cli",
-            "protontricks-launch = protontricks.cli.launch:cli",
-            # `protontricks-desktop-install` is only responsible for installing
-            # .desktop files and should be omitted if the distro package
-            # already ships .desktop files properly
-            ("protontricks-desktop-install "
-             "= protontricks.cli.desktop_install:cli")
-        ]
-    },
-    include_package_data=True,
-    license="GPL3",
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Topic :: Utilities',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
-    ],
+    ]
 )
